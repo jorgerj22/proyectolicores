1037ad709ab9ab82816fe38f0b86decfd21c7d6c
jorgerj22 <103084509+jorgerj22@users.noreply.github.com>
2022-11-05 20:09:17 -0300

    puliendodetalles

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,7 +1,7 @@
 /**
 * PHP Email Form Validation - v3.0
 * URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
+
 */
 (function () {
   "use strict";
--- forms/book-a-table.php
+++ forms/book-a-table.php
@@ -4,7 +4,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'jorgerj_22@hotmail.com';
+  $receiving_email_address = 'losdelvino10@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
